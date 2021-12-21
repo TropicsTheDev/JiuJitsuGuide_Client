f4f9e33f9eb9dff47f978dd5e6fe19ff46462385
Douglas Townsend <dougymt@gmail.com>
2021-12-20 23:44:43 -0500

    added apollo and normalize.css

--- source/pages/index.vue
+++ source/pages/index.vue
@@ -1,7 +1,34 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script>
-export default {}
+import gql from "graphql-tag"
+
+const TEST_RICK_AND_MORTY_QUERY = gql`
+  query {
+    characters {
+      info {
+        count
+      }
+      results {
+        name
+        status
+        species
+      }
+    }
+    location(id: 1) {
+      id
+    }
+    episodesByIds(ids: [1, 2]) {
+      id
+    }
+  }
+`
+export default {
+  apollo: {
+    characters: TEST_RICK_AND_MORTY_QUERY,
+    location: TEST_RICK_AND_MORTY_QUERY,
+  },
+}
 </script>
